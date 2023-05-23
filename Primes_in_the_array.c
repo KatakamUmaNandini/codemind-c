9edd087bf10c12382942dd820f76e962edfb4173
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-05-24 03:35:47 +0530

    #include<stdio.h> int prime(int n) {     int i,c=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else{         return 0;     } } int main() {     int n,c,count=0;     scanf("%d",&n);     int i,a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         c=prime(a[i]);         if(c==1)         {             count++;         }     }     printf("%d",count); }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int i,c=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+int main()
+{
+    int n,c,count=0;
+    scanf("%d",&n);
+    int i,a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=prime(a[i]);
+        if(c==1)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
