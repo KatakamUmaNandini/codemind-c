5eed83083bc503478d75b8f1a35f632adaac56cf
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-05-26 15:39:47 +0530

    #include<stdio.h> int main() {     int n,t,m,r=0,rev=0;     scanf("%d",&n);     int u=n;     int s=n*n;     while(n>0){         t=n%10;         r=(r*10)+t;         n=n/10;     }     int p=r*r;     while(p>0)     {         m=p%10;         rev=(rev*10)+m;         p=p/10;     }     if(rev==s)     {         printf("True");     }     else{         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,26 +1,27 @@
 #include<stdio.h>
-int main(){
-int n,s,t,r=0,q,rev=0,a;
-scanf("%d",&n);
-s=n*n;
-while(n!=0)
+int main()
 {
-    t=n%10;
-    r=(r*10)+t;
-    n=n/10;
-}
-q=r*r;
-while(q!=0)
-{
-    a=q%10;
-    rev=(rev*10)+a;
-    q=q/10;
-}
-if(rev==s)
-{
-    printf("True");
-}
-else{
-    printf("False");
-}
+    int n,t,m,r=0,rev=0;
+    scanf("%d",&n);
+    int u=n;
+    int s=n*n;
+    while(n>0){
+        t=n%10;
+        r=(r*10)+t;
+        n=n/10;
+    }
+    int p=r*r;
+    while(p>0)
+    {
+        m=p%10;
+        rev=(rev*10)+m;
+        p=p/10;
+    }
+    if(rev==s)
+    {
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
 }
