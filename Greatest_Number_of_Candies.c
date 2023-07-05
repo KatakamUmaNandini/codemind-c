d73e1aa915783135d7fe9e1470e0311e8453e9be
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-07-05 13:15:06 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int i,a[n];     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int k;     scanf("%d",&k);     int max=a[0];     for(i=0;i<n;i++)     {         if(a[i]>max)         {             max=a[i];         }     }     for(i=0;i<n;i++)     {         if(a[i]+k>=max)         {             printf("True ");         }         else{             printf("False ");         }     } }

--- Greatest_Number_of_Candies.c
+++ Greatest_Number_of_Candies.c
@@ -1,24 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int m,i;
-    scanf("%d",&m);
-    int a[m];
-    for(i=0;i<m;i++)
+    int n;
+    scanf("%d",&n);
+    int i,a[n];
+    for(i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
     int k;
     scanf("%d",&k);
     int max=a[0];
-     for(i=0;i<m;i++)
+    for(i=0;i<n;i++)
     {
         if(a[i]>max)
         {
-           max=a[i];   
+            max=a[i];
         }
     }
-     for(i=0;i<m;i++)
+    for(i=0;i<n;i++)
     {
         if(a[i]+k>=max)
         {
