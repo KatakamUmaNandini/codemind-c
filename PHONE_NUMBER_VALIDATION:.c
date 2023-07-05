9fb30ac2faa56c877f94a8c1b8e6b9186c5e604a
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-07-05 13:29:31 +0530

    #include<stdio.h> int main() {     char str[100];     scanf("%s",str);     int c=0,i;     for(i=0;str[i]!=0;i++)     {         c++;     }     if(str[0]!=0&&c==10)     {         printf("Valid");     }     else     {         printf("Invalid");     } }

--- PHONE_NUMBER_VALIDATION:.c
+++ PHONE_NUMBER_VALIDATION:.c
@@ -1,19 +1,14 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-    int c=0,i;
     char str[100];
     scanf("%s",str);
-    int len =strlen(str);
-    for(i=0;str[i]!=NULL;i++)
+    int c=0,i;
+    for(i=0;str[i]!=0;i++)
     {
-        if(str[0]==0)
-        {
-            c++;
-        }
+        c++;
     }
-    if(c==0 && len==10)
+    if(str[0]!=0&&c==10)
     {
         printf("Valid");
     }
