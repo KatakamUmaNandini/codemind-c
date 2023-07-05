5a174e42582421e5913d8246984328f31df4ec3b
KatakamUmaNandini <126311990+KatakamUmaNandini@users.noreply.github.com>
2023-07-05 13:32:17 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a=0,b=1,c,i=1;     while(i<=n)     {         printf("%d ",a);         c=a+b;         a=b;         b=c;         i+=1;     } }

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a=0,b=1,c,i=1;
+    while(i<=n)
+    {
+        printf("%d ",a);
+        c=a+b;
+        a=b;
+        b=c;
+        i+=1;
+    }
+}
